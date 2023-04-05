9a557f5b65eee9d8aa5fc46cef46ac05490ae9cd
wasifa-khan <wasifaasadk@gmail.com>
2023-04-05 16:08:56 +0500

    Chapter 1

---
+++ Chapter 1/app.js
@@ -0,0 +1,15 @@
+// Question 1
+//alert("Welcome to my Website");
+
+//Question 2
+//alert("Error! Please enter a valid password.");
+
+//Question 3
+//alert("Welcome to JS Land... \n Happy Coding!");
+
+//Question 4
+// alert("Welcome to JS Land...");
+// alert("Happy Coding! \nPrevent this page from creating additional dialogs.")
+
+//Question 5
+alert("Hello... I can run JS through my web browser's console")
---
+++ Chapter 2/app.js
@@ -0,0 +1,37 @@
+//Question 1
+let username;
+
+//Question 2
+let myName = "Wasifa Khan";
+
+//Question 3
+let titledMessage = "Hello World";
+alert(titledMessage);
+
+//Question 4
+let fullName = "Wasifa Khan";
+let age = 20 + "years old";
+let course = "Certified Mobile Application Development";
+alert(fullName);
+alert(age);
+alert(course);
+
+//Question 5
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+//Question 6
+let email = "wasifaasadk@gmail.com";
+alert("My email address is " + email);
+
+//Question 7
+let book = "A smarter \nway to learn Javascript";
+alert("I am trying to learn from the Book " + book);
+
+//Question 8
+document.write("Yah! I can write HTML content through JavaScript");
+
+//Question 9
+let pattern = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(pattern);
+
+
